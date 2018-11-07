6604c84300f9f5daecc0d66671e94d20911c358c
Taran Singh <taran.jhattu@gmail.com>
2018-11-07 09:03:06 +0100

    Javascriptbasics-Taran

--- javascript.js
+++ javascript.js
@@ -1,1 +1,18 @@
 console.log("Hello World");
+
+var a = 10;
+if (a > 9) {
+    console.log("Hallo a is groter dan 9 hoor");
+}
+
+for (var i = 0; i < 10; i++) {
+    console.log("i heeft nu de waarde " + i);
+}
+var mijnauto= {
+    merk: "Ford",
+    type: "Mondeo",
+    aantalwielen: 4,
+    kleur: "Blauw"
+}
+
+console.log(mijnauto.kleur);
